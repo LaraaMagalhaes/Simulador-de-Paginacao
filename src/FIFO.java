feedf64bad931f8a0840ceb37e98a04febdc466b
AnnaLara <annalarammv123@gmail.com>
2025-10-30 20:54:30 -0300

    Implementação FIFO

--- src/FIFO.java
+++ src/FIFO.java
@@ -3,9 +3,25 @@ import java.util.LinkedList;
 import java.util.HashSet; 
 
 public class FIFO {
+    //Lógica: Remove a página que está na memória há mais tempo.
     public int simular(int[] paginas, int numQuadros) {
+        //Utiliza a fila para dizer quem será o primeiro a ser removido e o conjunto hashSet diz se uma página já está na memória
         int faltasDePagina = 0;
-
+        Queue<Integer> quadrosFila = new LinkedList<>();
+        HashSet<Integer> quadrosSet = new HashSet<>();
+        for (int paginaAtual : paginas) {
+            if (!quadrosSet.contains(paginaAtual)) {
+                faltasDePagina++;
+                if (quadrosFila.size() == numQuadros) {
+                    int paginaRemovida = quadrosFila.poll();
+                    quadrosSet.remove(paginaRemovida);
+                }
+                quadrosFila.add(paginaAtual);
+                quadrosSet.add(paginaAtual);
+                
+            } else {
+            }
+        } 
         return faltasDePagina;
     }
 }
